4b23b3edb74ecb757c7b56dd38a099786d5f383a
Deejpotter <deejpotter@gmail.com>
2021-06-29 21:41:34 +1000

    Updated some stuff

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ class Square extends React.Component {
 
   render() {
     return (
-      <button className="square" onClick={() => { this.setState({value: 'X'}) }}>
+      <button className="square" onClick={() => { this.setState({ value: 'X' }) }}>
         {this.state.value}
       </button>
     );
@@ -20,13 +20,13 @@ class Square extends React.Component {
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    this.state - {
+    this.state = {
       squares: Array(9).fill(null),
     };
   }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]} />;
   }
 
   render() {
